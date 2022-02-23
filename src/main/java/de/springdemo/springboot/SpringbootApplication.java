8d490410f1c37375df01077bdaf095163e443cc4
enextus <enextus@gmail.com>
2022-02-23 15:37:02 +0100

    run

--- src/main/java/de/springdemo/springboot/SpringbootApplication.java
+++ src/main/java/de/springdemo/springboot/SpringbootApplication.java
@@ -2,14 +2,14 @@ package de.springdemo.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController // not the @Controller
 @SpringBootApplication
 public class SpringbootApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
 
