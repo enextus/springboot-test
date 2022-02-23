3be6129d1b0743a3522ed02278174943ccbb3001
enextus <enextus@gmail.com>
2022-02-23 13:33:36 +0100

    add mapping

--- src/main/java/de/springdemo/springboot/SpringbootApplication.java
+++ src/main/java/de/springdemo/springboot/SpringbootApplication.java
@@ -2,12 +2,20 @@ package de.springdemo.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+	@RequestMapping("/hello")
+	public String showHelloWorld() {
+		return "hello-world";
 	}
 
 }
