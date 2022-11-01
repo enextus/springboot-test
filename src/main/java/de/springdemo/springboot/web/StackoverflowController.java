1ebbbb2c9de383b3fc9347b1992b174528af8213
enextus <enextus@gmail.com>
2022-11-01 20:14:52 +0100

    pom

--- src/main/java/de/springdemo/springboot/web/StackoverflowController.java
+++ src/main/java/de/springdemo/springboot/web/StackoverflowController.java
@@ -5,15 +5,17 @@ import java.util.List;
 import de.springdemo.springboot.model.StackoverflowWebsite;
 import de.springdemo.springboot.service.StackoverflowService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/stackoverflow")
 public class StackoverflowController {
-	@Autowired
-	private StackoverflowService stackoverflowService;
+	private final StackoverflowService stackoverflowService;
+
+	public StackoverflowController(StackoverflowService stackoverflowService) {
+		this.stackoverflowService = stackoverflowService;
+	}
 
 	@RequestMapping
 	public List<StackoverflowWebsite> getListOfProviders() {
